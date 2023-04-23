7b674a66acc13c3ba469c0c1f9d11d0b48dff6b1
jmfarthin <122315957+jmfarthin@users.noreply.github.com>
2023-04-23 12:28:38 -0400

    added delete route for Tags

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -63,8 +63,14 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({ where: { id: req.params.id } });
+    res.status(200).json(deleteTag);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 module.exports = router;
