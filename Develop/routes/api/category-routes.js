3f9a851294f302ccb68b57f2c60d13c65b8f7290
jmfarthin <122315957+jmfarthin@users.noreply.github.com>
2023-04-23 13:30:22 -0400

    added post, put, delete routes for Category and updated readme sections description, usage, credits

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,26 +3,61 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const allCategories = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(allCategories);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    res.status(200).json(category);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(req.body, { where: { id: req.params.id } });
+    res.status(200).json(updateCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteCategory = await Category.destroy({ where: { id: req.params.id } });
+    res.status(200).json(deleteCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # E-commerce Back End Starter Code
+This repo contains code for an ecommerce backend.
+
+## Description
+
+This ecommerce backend application allows you to perform CRUD functions for each of the main tables: Category, Product, and Tag. It's purpose is to showcase my current knowledge of javascript and highlight certain proficiencies such as:
+
+- NODEjs
+- Sequelize
+- Models
+- ORM
+- CRUD operations
+
+## Usage
+
+Usage of this application assumes the following: that you have cloned the repo, have node, mysql installed.
+
+Then, from the command line:
+
+- 'npm i' to install depedencies
+- run the schema.sql and seed the database using seeds.sql 
+- 'node index.js' to run application
+
+You will know that the application is running when you see the following message in the console: 'App listening on port 3001!'
+
+To test routes, use your favorite API tester, e.g., Insomnia.
+
+Repository: https://github.com/jmfarthin/e-commerce-backend
+
+### Video of application in use:
+
+[Video Demonstration]()
+
+
+## Tests
+
+Currently no tests.
+
+# Credits
+
+Alexis Gonzalez for his help with the put tag route and updating the post product route to async.
+
+## License
+
+Licensed under the general usage MIT license.
+Copyright (c) 2023 Justin Farthing
